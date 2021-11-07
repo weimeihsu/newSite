35adbfaf896201739e2fbdf3a5b19a4993330e01
weimei <weimei.hsu@essist.com.tw>
2021-11-07 22:09:34 +0800

    install vuex

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex';
+
+// create store
+const store =  createStore({
+    state: { },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+    }
+  })
+  export default store;
